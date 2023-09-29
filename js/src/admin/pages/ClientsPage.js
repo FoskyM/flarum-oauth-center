5a8ea7f751b7974d52b50f511207dc3928490946
FoskyM <i@fosky.top>
2023-09-29 21:58:04 +0800

    feat(admin): list clients api

--- js/src/admin/pages/ClientsPage.js
+++ js/src/admin/pages/ClientsPage.js
@@ -1,11 +1,22 @@
+import app from 'flarum/admin/app';
 import Page from 'flarum/common/components/Page';
 
 export default class ClientsPage extends Page {
-    view() {
-        return (
-            <div>
-                <h2>Clients Page</h2>
-            </div>
-        );
-    }
+  settingName = 'collapsible-posts.reasons';
+  translationPrefix = 'foskym-oauth-center.admin.clients.';
+  oninit(vnode) {
+    super.oninit(vnode);
+
+    app.store.find('clients').then(() => {
+      m.redraw();
+    });
+  }
+
+  view() {
+    return (
+      <div>
+        <h2>Clients Page</h2>
+      </div>
+    );
+  }
 }
