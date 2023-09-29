56dd00e200e7ae17dc497a82a88c19b962bbd94f
FoskyM <i@mouse123.cn>
2023-09-29 15:26:42 +0800

    feat(admin): setting page

---
+++ js/src/admin/pages/ClientsPage.js
@@ -0,0 +1,11 @@
+import Page from 'flarum/common/components/Page';
+
+export default class ClientsPage extends Page {
+    view() {
+        return (
+            <div>
+                <h2>Clients Page</h2>
+            </div>
+        );
+    }
+}
