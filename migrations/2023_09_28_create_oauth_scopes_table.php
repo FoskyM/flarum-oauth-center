386664042e8169886470e761ae8103cd540c5406
FoskyM <i@fosky.top>
2023-09-29 09:44:17 +0800

    chore: create the scope middleware

--- migrations/2023_09_28_create_oauth_scopes_table.php
+++ migrations/2023_09_28_create_oauth_scopes_table.php
@@ -11,6 +11,8 @@ return [
         $schema->create('oauth_scopes', function (Blueprint $table) {
             $table->increments('id');
             $table->string('scope', 80);
+            $table->string('resource_path', 500);
+            $table->string('method', 20);
             $table->boolean('is_default')->nullable();
         });
     },
--- src/Models/Scope.php
+++ src/Models/Scope.php
@@ -7,4 +7,10 @@ use Flarum\Database\AbstractModel;
 class Scope extends AbstractModel
 {
     protected $table = 'oauth_scopes';
+
+    static public function get_path_scope($path = '')
+    {
+        return self::where('resource_path', 'like', $path . '%')->first();
+        // return $this->where('resource_path', $path)->first();
+    }
 }
