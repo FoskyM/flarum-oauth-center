3b5354c4aff5e3c7d916673123b2800af62acaee
FoskyM <i@fosky.top>
2023-10-01 19:54:19 +0800

    fix: scope model

--- js/src/common/models/Scope.js
+++ js/src/common/models/Scope.js
@@ -2,6 +2,8 @@ import Model from 'flarum/common/Model';
 
 export default class Client extends Model {
   scope = Model.attribute('scope');
+  resource_path = Model.attribute('resource_path');
+  method = Model.attribute('method');
   is_default = Model.attribute('is_default', value => value === 1);
   scope_name = Model.attribute('scope_name');
   scope_icon = Model.attribute('scope_icon');
