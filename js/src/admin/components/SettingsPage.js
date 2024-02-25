1cb2755e019b21cdf52c16f502560d8a63ca4a75
FoskyM <i@fosky.top>
2024-02-25 14:42:39 +0800

    perf: settings page

--- js/src/admin/components/SettingsPage.js
+++ js/src/admin/components/SettingsPage.js
@@ -4,67 +4,64 @@ import Button from 'flarum/common/components/Button';
 import IndexPage from "../pages/IndexPage";
 import ClientsPage from "../pages/ClientsPage";
 import ScopesPage from "../pages/ScopesPage";
+
 export default class SettingsPage extends ExtensionPage {
-    content() {
-        const page = m.route.param().page || 'index';
+  translationPrefix = 'foskym-oauth-center.admin.page.';
+  pages = {
+    index: IndexPage,
+    clients: ClientsPage,
+    scopes: ScopesPage
+  };
 
-        return (
-            <div className="ExtensionPage-settings OAuthCenter">
-                <div className={"oauth-menu"}>
-                    <div className={"container"}>
-                        {this.menuButtons(page)}
-                    </div>
-                </div>
+  content() {
+    const page = m.route.param().page || 'index';
 
-                <div className="container OAuthCenterPage-container">
-                    {this.pageContent(page)}
-                </div>
-            </div>
-        );
+    return (
+      <div className="ExtensionPage-settings OAuthCenter">
+        <div className={"oauth-menu"}>
+          <div className={"container"}>
+            {this.menuButtons(page)}
+          </div>
+        </div>
+
+        <div className="container OAuthCenterPage-container">
+          {this.pageContent(page)}
+        </div>
+      </div>
+    );
+  }
+
+  // Return button menus
+  menuButtons(page) {
+    return Object.keys(this.pages).map(key =>
+      Button.component({
+        className: `Button ${page === key ? 'item-selected' : ''}`,
+        onclick: () => m.route.set(
+          app.route('extension', {
+            id: 'foskym-oauth-center',
+            page: key
+          })
+        ),
+        icon: this.iconForPage(key),
+      }, app.translator.trans(this.translationPrefix + key))
+    );
+  }
+
+  iconForPage(page) {
+    switch (page) {
+      case 'index':
+        return 'fas fa-home';
+      case 'clients':
+        return 'fas fa-network-wired';
+      case 'scopes':
+        return 'fas fa-user-lock';
+      default:
+        return '';
     }
+  }
 
-    // Return button menus
-    menuButtons(page) {
-        return [
-            Button.component({
-                className: `Button ${page === 'index' ? 'item-selected' : ''}`,
-                onclick: () => m.route.set(
-                    app.route('extension', {
-                        id: 'foskym-oauth-center'
-                    })
-                ),
-                icon: 'fas fa-home',
-            }, app.translator.trans('foskym-oauth-center.admin.page.index')),
-            Button.component({
-                className: `Button ${page === 'clients' ? 'item-selected' : ''}`,
-                onclick: () => m.route.set(
-                    app.route('extension', {
-                        id: 'foskym-oauth-center',
-                        page: 'clients'
-                    })
-                ),
-                icon: 'fas fa-network-wired',
-            }, app.translator.trans('foskym-oauth-center.admin.page.clients')),
-            Button.component({
-                className: `Button ${page === 'scopes' ? 'item-selected' : ''}`,
-                onclick: () => m.route.set(
-                    app.route('extension', {
-                        id: 'foskym-oauth-center',
-                        page: 'scopes'
-                    })
-                ),
-                icon: 'fas fa-user-lock',
-            }, app.translator.trans('foskym-oauth-center.admin.page.scopes')),
-        ];
-    }
-
-
-    pageContent(page) {
-        if (page == 'clients') {
-            return <ClientsPage />
-        } else if (page == 'scopes') {
-            return <ScopesPage />
-        }
-        return <IndexPage />
-    }
+  pageContent(page) {
+    const PageComponent = this.pages[page];
+    return PageComponent ? <PageComponent/> : null;
+  }
 }
