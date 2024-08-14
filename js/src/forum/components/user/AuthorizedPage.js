f0ba01e9b057396e7f4d194c5c51788d1b1020ab
FoskyM <i@fosky.top>
2024-08-14 12:26:55 +0800

    feat(authorized page): client icon & link

--- js/src/forum/components/user/AuthorizedPage.js
+++ js/src/forum/components/user/AuthorizedPage.js
@@ -15,7 +15,7 @@ export default class AuthorizedPage extends UserPage {
     this.loadRecords();
   }
   loadRecords() {
-    app.store.find('oauth-records', {page: this.page}).then(records => {
+    app.store.find('oauth-records', { page: this.page }).then((records) => {
       this.records = this.records.concat(records);
       this.loading = false;
       if (records.length < 10) {
@@ -25,46 +25,48 @@ export default class AuthorizedPage extends UserPage {
     });
   }
   loadMore() {
-    this.loadRecords(this.page += 1);
+    this.loadRecords((this.page += 1));
   }
   content() {
     if (this.records.length === 0) {
-      return <Placeholder text={app.translator.trans('foskym-oauth-center.forum.authorized.no_records')}/>;
+      return <Placeholder text={app.translator.trans('foskym-oauth-center.forum.authorized.no_records')} />;
     }
 
     return (
       <div className="AuthorizedPage">
         <ul className="AuthorizedRecords">
-          {this.records.map(record =>
+          {this.records.map((record) => (
             <li className="AuthorizedRecord">
-              <h3>
-                {record.attribute('client').client_name}
-                <time>
-                  {record.authorized_at().toLocaleString()}
-                </time>
-              </h3>
-              <p>{record.attribute('client').client_desc}</p>
+              <div className="AuthorizedRecord-content">
+                <div className="AuthorizedRecord-left">
+                  <img className="AuthorizedRecord-icon" src={record.attribute('client').client_icon} alt="client_icon" />
+                  <div className="AuthorizedRecord-info">
+                    <h3>
+                      <a href={record.attribute('client').client_home} target="_blank">{record.attribute('client').client_name}</a>
+                    </h3>
+                    <p>{record.attribute('client').client_desc}</p>
+                  </div>
+                </div>
+                <div className="AuthorizedRecord-right">
+                  <time>{record.authorized_at().toLocaleString()}</time>
+                </div>
+              </div>
               <hr />
             </li>
-          )}
+          ))}
         </ul>
 
-        {this.loading && (
-          <LoadingIndicator display="block"/>
-        )}
+        {this.loading && <LoadingIndicator display="block" />}
 
         {!this.loading && !this.nomore && (
           <div style="text-align:center;padding:20px">
-            <Button className={'Button Button--primary'} disabled={this.loading} loading={this.loading}
-                    onclick={() => this.loadMore()}>
+            <Button className={'Button Button--primary'} disabled={this.loading} loading={this.loading} onclick={() => this.loadMore()}>
               {app.translator.trans('foskym-oauth-center.forum.authorized.load_more')}
             </Button>
           </div>
         )}
 
-        {this.nomore && (
-          <Placeholder text={app.translator.trans('foskym-oauth-center.forum.authorized.no_more_records')}/>
-        )}
+        {this.nomore && <Placeholder text={app.translator.trans('foskym-oauth-center.forum.authorized.no_more_records')} />}
       </div>
     );
   }
